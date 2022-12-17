58212ec3d3bef54bc895d876e1f54e9243b60867
Masayoshi347 <90193223+Masayoshi347@users.noreply.github.com>
2022-12-17 19:32:41 +0100

    a

--- main/test/ScientificCalculatorTest.java
+++ main/test/ScientificCalculatorTest.java
@@ -1,39 +1,37 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import main.src.Calculatrice;
 
 public class ScientificCalculatorTest {
   @Test
   public void testAddition() {
-    ScientificCalculator calculator = new ScientificCalculator();
+    Calculatrice calculator = new Calculatrice();
     double result = calculator.add(5, 6);
     assertEquals(11, result, 0);
   }
 
   @Test
   public void testSoustraction() {
-    ScientificCalculator calculator = new ScientificCalculator();
+    Calculatrice calculator = new Calculatrice();
     double result = calculator.subtract(5, 6);
     assertEquals(-1, result, 0);
   }
 
   @Test
   public void testMultiplication() {
-    ScientificCalculator calculator = new ScientificCalculator();
+    Calculatrice calculator = new Calculatrice();
     double result = calculator.multiply(5, 6);
     assertEquals(30, result, 0);
   }
 
   @Test
   public void testDivision() {
-    ScientificCalculator calculator = new ScientificCalculator();
+    Calculatrice calculator = new Calculatrice();
     double result = calculator.divide(5, 6);
     assertEquals(0.8333333333333334, result, 0);
   }
 
   @Test
   public void testModulo() {
-    ScientificCalculator calculator = new ScientificCalculator();
+    ScientificCalculator calculator = new Calculatrice();
     double result = calculator.modulo(5, 6);
     assertEquals(5, result, 0);
   }
